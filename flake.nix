8f6954a761e27660b4bfee7bb941c01e5f6aac62
Clay <clay@clay.rip>
2026-04-12 22:25:59 -0400

    bump vencord and equicord versions

--- flake.nix
+++ flake.nix
@@ -153,7 +153,7 @@
 
 				recar = pkgs.stdenv.mkDerivation rec {
 					pname = "recar";
-					version = "1.1.16";
+					version = "1.1.17";
 
 					src = pkgs.runCommand "recar-src" { } ''
 						mkdir -p $out
