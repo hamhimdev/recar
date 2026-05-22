e1c229cd938b85ca60bfe7fdbb77a882d4b929bf
Clay <clay@clay.rip>
2026-05-22 00:34:59 -0400

    bump version

--- flake.nix
+++ flake.nix
@@ -153,7 +153,7 @@
 
 				recar = pkgs.stdenv.mkDerivation rec {
 					pname = "recar";
-					version = "1.1.19";
+					version = "1.1.20";
 
 					src = pkgs.runCommand "recar-src" { } ''
 						mkdir -p $out
